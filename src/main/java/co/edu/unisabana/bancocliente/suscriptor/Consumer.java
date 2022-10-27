189fec6f8fc03eb10fe148993ab721cc49449644
Dani Saavedra <daniel.saavedra@mercadolibre.com.co>
2022-10-26 20:00:26 -0500

    Implementa lectura de objeto pago

--- src/main/java/co/edu/unisabana/bancocliente/suscriptor/Consumer.java
+++ src/main/java/co/edu/unisabana/bancocliente/suscriptor/Consumer.java
@@ -1,22 +1,31 @@
 package co.edu.unisabana.bancocliente.suscriptor;
 
+import co.edu.unisabana.bancocliente.suscriptor.data.Pago;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
 
 @Component
 @Slf4j
 public class Consumer {
 
     @RabbitListener(queues = {"pagosEjecutados"})
-    public void receive(@Payload String message) {
-        log.info("Voy a procesasr el mensaje: {}", message);
+    public void receive(@Payload Pago pago) {
+        log.info("Voy a procesasr el mensaje: {}", pago.getMonto());
+
+        /**
+         * Esto de aca abajo simula que la escucha del mensaje se demora 10 segundos.
+         *    :)
+         */
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        log.info("Procesaimiento Terminado del mensaje: {}", message);
+        log.info("Procesaimiento Terminado del mensaje: {}", pago);
     }
+
 }
---
+++ src/main/java/co/edu/unisabana/bancocliente/suscriptor/data/Pago.java
@@ -0,0 +1,40 @@
+package co.edu.unisabana.bancocliente.suscriptor.data;
+
+import java.io.Serializable;
+
+public class Pago implements Serializable {
+
+    private int monto;
+    private String cuentaOrigen;
+    private String cuentaDestino;
+
+    public Pago(int monto, String cuentaOrigen, String cuentaDestino) {
+        this.monto = monto;
+        this.cuentaOrigen = cuentaOrigen;
+        this.cuentaDestino = cuentaDestino;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getCuentaOrigen() {
+        return cuentaOrigen;
+    }
+
+    public void setCuentaOrigen(String cuentaOrigen) {
+        this.cuentaOrigen = cuentaOrigen;
+    }
+
+    public String getCuentaDestino() {
+        return cuentaDestino;
+    }
+
+    public void setCuentaDestino(String cuentaDestino) {
+        this.cuentaDestino = cuentaDestino;
+    }
+}
