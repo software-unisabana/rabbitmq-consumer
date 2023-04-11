2ef21e33efb5912cacb965e7521984170f5182e4
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-04-10 21:07:25 -0500

    Delete class pago in work only with string

--- src/main/java/co/edu/unisabana/bancocliente/suscriptor/Consumer.java
+++ src/main/java/co/edu/unisabana/bancocliente/suscriptor/Consumer.java
@@ -1,6 +1,5 @@
 package co.edu.unisabana.bancocliente.suscriptor;
 
-import co.edu.unisabana.pagos.controller.Pago;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,8 +9,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class Consumer {
 
-    @RabbitListener(queues = {"unisabana.pagos.notificar"})
-    public void receive(@Payload Pago pago) {
-        log.info("Escuche de la cola unisabana.pagos.notificar {}", pago.getMonto());
+    @RabbitListener(queues = {"${unisabana.pagos.notificar}"})
+    public void receive(@Payload String pago) {
+        log.info("Escuche de la cola unisabana.pagos.notificar {}", pago);
     }
 }
